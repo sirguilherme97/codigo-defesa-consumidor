098c9be9e418d3ac968ce9250a199228a875d12b
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-06-28 03:36:59 -0300

    Titulo II pronto

--- src/Components/Titulos/T2.tsx
+++ src/Components/Titulos/T2.tsx
@@ -4,11 +4,100 @@ export function T2() {
         <div className='w-screen px-20 mt-12 flex items-center justify-start '>
             <Popover>
                 <Popover.Button className='flex flex-col items-start justify-center '>
-                    <h1 className='text-xl font-bold'>Titulo II</h1>
+                    <h1 className='text-xl font-bold text-rep'>Titulo II</h1>
                     <p className='text-lg'>Das Infrações Penais</p>
                 </Popover.Button>
-                <Popover.Panel>
-                    <h1>Oque_vai_aparecer_no_Painel</h1>
+                <Popover.Panel className='mt-4 ml-6'>
+                    <p><strong>Art. 61.</strong> Constituem crimes contra as relações de consumo previstas neste código, sem prejuízo do disposto no Código Penal e leis especiais, as condutas tipificadas nos artigos seguintes.</p>
+                    <p><strong>Art. 62.</strong> (Vetado).</p>
+                    <p><strong>Art. 63.</strong> Omitir dizeres ou sinais ostensivos sobre a nocividade ou periculosidade de produtos, nas embalagens, nos invólucros, recipientes ou publicidade:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de seis meses a dois anos e multa.</p>
+                        <p><strong>§ 1°</strong> Incorrerá nas mesmas penas quem deixar de alertar, mediante recomendações escritas ostensivas, sobre a periculosidade do serviço a ser prestado.</p>
+                        <p><strong>§ 2°</strong> Se o crime é culposo:</p>
+                        <p><strong>Pena -</strong> Detenção de um a seis meses ou multa.</p>
+                    </div>
+                    <p><strong>Art. 64.</strong> Deixar de comunicar à autoridade competente e aos consumidores a nocividade ou periculosidade de produtos cujo conhecimento seja posterior à sua colocação no mercado:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de seis meses a dois anos e multa.</p>
+                        <p>Parágrafo único. Incorrerá nas mesmas penas quem deixar de retirar do mercado, imediatamente quando determinado pela autoridade competente, os produtos nocivos ou perigosos, na forma deste artigo.</p>
+                    </div>
+                    <p><strong>Art. 65.</strong> Executar serviço de alto grau de periculosidade, contrariando determinação de autoridade competente:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de seis meses a dois anos e multa.</p>
+                        <p><strong>§ 1º</strong> As penas deste artigo são aplicáveis sem prejuízo das correspondentes à lesão corporal e à morte. <a href="http://www.planalto.gov.br/ccivil_03/_Ato2015-2018/2017/Lei/L13425.htm#art18" target='_blank'>(Redação dada pela Lei nº 13.425, de 2017)</a></p>
+                        <p><strong>§ 2º</strong> A prática do disposto no inciso XIV do art. 39 desta Lei também caracteriza o crime previsto no caput deste artigo. <a href="http://www.planalto.gov.br/ccivil_03/_Ato2015-2018/2017/Lei/L13425.htm#art18" target='_blank'>(Incluído pela Lei nº 13.425, de 2017)</a></p>
+                    </div>
+                    <p><strong>Art. 66.</strong> Fazer afirmação falsa ou enganosa, ou omitir informação relevante sobre a natureza, característica, qualidade, quantidade, segurança, desempenho, durabilidade, preço ou garantia de produtos ou serviços:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de três meses a um ano e multa.</p>
+                        <p><strong>§ 1º</strong> Incorrerá nas mesmas penas quem patrocinar a oferta.</p>
+                        <p><strong>§ 2º</strong> Se o crime é culposo;</p>
+                        <p><strong>Pena -</strong> Detenção de um a seis meses ou multa.</p>
+                    </div>
+                    <p><strong>Art. 67.</strong> Fazer ou promover publicidade que sabe ou deveria saber ser enganosa ou abusiva:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de três meses a um ano e multa.</p>
+                        <p>Parágrafo único. (Vetado).</p>
+                    </div>
+                    <p><strong>Art. 68.</strong> Fazer ou promover publicidade que sabe ou deveria saber ser capaz de induzir o consumidor a se comportar de forma prejudicial ou perigosa a sua saúde ou segurança:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de seis meses a dois anos e multa:</p>
+                        <p>Parágrafo único. (Vetado).</p>
+                    </div>
+                    <p><strong>Art. 69.</strong> Deixar de organizar dados fáticos, técnicos e científicos que dão base à publicidade:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de um a seis meses ou multa.</p>
+                    </div>
+                    <p><strong>Art. 70.</strong> Empregar na reparação de produtos, peça ou componentes de reposição usados, sem autorização do consumidor:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de três meses a um ano e multa.</p>
+                    </div>
+                    <p><strong>Art. 71.</strong> Utilizar, na cobrança de dívidas, de ameaça, coação, constrangimento físico ou moral, afirmações falsas incorretas ou enganosas ou de qualquer outro procedimento que exponha o consumidor, injustificadamente, a ridículo ou interfira com seu trabalho, descanso ou lazer:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de três meses a um ano e multa.</p>
+                    </div>
+                    <p><strong>Art. 72.</strong> Impedir ou dificultar o acesso do consumidor às informações que sobre ele constem em cadastros, banco de dados, fichas e registros:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de seis meses a um ano ou multa.</p>
+                    </div>
+                    <p><strong>Art. 73.</strong> Deixar de corrigir imediatamente informação sobre consumidor constante de cadastro, banco de dados, fichas ou registros que sabe ou deveria saber ser inexata:</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de um a seis meses ou multa.</p>
+                    </div>
+                    <p><strong>Art. 74.</strong> Deixar de entregar ao consumidor o termo de garantia adequadamente preenchido e com especificação clara de seu conteúdo;</p>
+                    <div className='ml-6'>
+                        <p><strong>Pena -</strong> Detenção de um a seis meses ou multa.</p>
+                    </div>
+                    <p><strong>Art. 75.</strong> Quem, de qualquer forma, concorrer para os crimes referidos neste código, incide as penas a esses cominadas na medida de sua culpabilidade, bem como o diretor, administrador ou gerente da pessoa jurídica que promover, permitir ou por qualquer modo aprovar o fornecimento, oferta, exposição à venda ou manutenção em depósito de produtos ou a oferta e prestação de serviços nas condições por ele proibidas.</p>
+                    <p><strong>Art. 76.</strong> São circunstâncias agravantes dos crimes tipificados neste código:</p>
+                    <div className='ml-6'>
+                        <p><strong>I -</strong> serem cometidos em época de grave crise econômica ou por ocasião de calamidade;</p>
+                        <p><strong>II -</strong> ocasionarem grave dano individual ou coletivo;</p>
+                        <p><strong>III -</strong> dissimular-se a natureza ilícita do procedimento;</p>
+                        <p><strong>IV -</strong> quando cometidos:</p>
+                        <div className='ml-6'>
+                            <p><strong>a)</strong> por servidor público, ou por pessoa cuja condição econômico-social seja manifestamente superior à da vítima;</p>
+                            <p><strong>b)</strong> em detrimento de operário ou rurícola; de menor de dezoito ou maior de sessenta anos ou de pessoas portadoras de deficiência mental interditadas ou não;</p>
+                        </div>
+                        <p><strong>V -</strong> serem praticados em operações que envolvam alimentos, medicamentos ou quaisquer outros produtos ou serviços essenciais .</p>
+                    </div>
+                    <p><strong>Art. 77.</strong> A pena pecuniária prevista nesta Seção será fixada em dias-multa, correspondente ao mínimo e ao máximo de dias de duração da pena privativa da liberdade cominada ao crime. Na individualização desta multa, o juiz observará o disposto no art. 60, §1° do Código Penal.</p>
+                    <p><strong>Art. 78.</strong> Além das penas privativas de liberdade e de multa, podem ser impostas, cumulativa ou alternadamente, observado odisposto nos arts. 44 a 47, do Código Penal:</p>
+                    <div className='ml-6'>
+                        <p><strong>I -</strong> a interdição temporária de direitos;</p>
+                        <p><strong>II -</strong> a publicação em órgãos de comunicação de grande circulação ou audiência, às expensas do condenado, de notícia sobre os fatos e a condenação;</p>
+                        <p><strong>III -</strong> a prestação de serviços à comunidade.</p>
+                    </div>
+                    <p><strong>Art. 79.</strong> O valor da fiança, nas infrações de que trata este código, será fixado pelo juiz, ou pela autoridade que presidir o inquérito, entre cem e duzentas mil vezes o valor do Bônus do Tesouro Nacional (BTN), ou índice equivalente que venha a substituí-lo.</p>
+                    <div className='ml-6'>
+                        <p>Parágrafo único. Se assim recomendar a situação econômica do indiciado ou réu, a fiança poderá ser:</p>
+                        <div className='ml-6'>
+                            <p><strong>a)</strong> reduzida até a metade do seu valor mínimo;</p>
+                            <p><strong>b)</strong> aumentada pelo juiz até vinte vezes.</p>
+                        </div>
+                    </div>
+                    <p><strong>Art. 80.</strong> No processo penal atinente aos crimes previstos neste código, bem como a outros crimes e contravenções que envolvam relações de consumo, poderão intervir, como assistentes do Ministério Público, os legitimados indicados no art. 82, inciso III e IV, aos quais também é facultado propor ação penal subsidiária, se a denúncia não for oferecida no prazo legal.</p>
                 </Popover.Panel>
             </Popover>
         </div>
