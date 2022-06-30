5c46e28e61f5f217afbfd4cf62240fe5c0432339
SirGuilhermeBR <sir_guilherme@hotmail.com>
2022-06-30 00:33:30 -0300

    Config Search Spacing

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -26,14 +26,14 @@ export function Search() {
                 <Dialog.Panel className='bg-zinc-50 z-[500] overflow-x-hidden overflow-y-auto pb-12 absolute top-0 left-0 w-screen h-screen py-0 leading-relaxed'>
                     <Header />
                     <div className='flex flex-col gap-4 px-20 py-12'>
-                        <Dialog.Title className='text-2xl font-medium text-zinc-800'>Pesquisar Artigo</Dialog.Title>
+                        <Dialog.Title className='text-2xl font-medium text-zinc-900'>Pesquisar Artigo</Dialog.Title>
                         <Dialog.Description className='text-zinc-700'>
                             Se você deseja buscar um artigo específico, indique-o na busca abaixo
                         </Dialog.Description>
                         <input
                             value={busca}
                             placeholder="Ex: Art 72"
-                            className="max-w-[200px] px-2 py-2 bg-zinc-300 border-2 border-rep text-zinc-900 placeholder:text-zinc-900"
+                            className="max-w-[200px] px-2 py-2 bg-zinc-200 border-2 border-rep text-zinc-900 placeholder:text-zinc-700"
                             onChange={(ev) => setBusca(ev.target.value)}
                             type="text" />
 
@@ -50,7 +50,7 @@ export function Search() {
                             <div className="px-12 font-medium text-zinc-900">
                                 <ul>
                                     {ListaFiltrada.map((lista: any) => (
-                                        <li key={lista}>{lista}</li>
+                                        <li className="mb-4 text-zinc-900 leading-relaxed" key={lista}>{lista}</li>
                                     ))}
                                 </ul>
                             </div>
